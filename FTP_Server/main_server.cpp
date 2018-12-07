dbf1b5636526fe8a44ed13d8aa082de878b96cf5
Burke Masles <burkemasles@yahoo.com>
2018-12-06 16:29:22 -0800

    Combination of Sample and File Split
    Used the sample from the MS website as a base and then was able to implement an image send in main_client.cpp and an imagefile recieve in main_server.cpp

--- FTP_Server/main_server.cpp
+++ FTP_Server/main_server.cpp
@@ -1,10 +1,213 @@
+#undef UNICODE
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include <stdlib.h>
 #include <stdio.h>
 
-#pragma comment(lib, "Ws2_32.lib")
+// Need to link with Ws2_32.lib
+#pragma comment (lib, "Ws2_32.lib")
+// #pragma comment (lib, "Mswsock.lib")
 
-int main()
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT "27015"
+
+bool readdata(SOCKET sock, void *buf, int buflen)
 {
-	return 0;
+    unsigned char *pbuf = (unsigned char *) buf;
+
+    while (buflen > 0)
+    {
+        int num = recv(sock, reinterpret_cast<char*>(pbuf), buflen, 0);
+        if (num == SOCKET_ERROR)
+        {
+            if (WSAGetLastError() == WSAEWOULDBLOCK)
+            {
+                // optional: use select() to check for timeout to fail the read
+                continue;
+            }
+            return false;
+        }
+        else if (num == 0)
+            return false;
+
+        pbuf += num;
+        buflen -= num;
+    }
+
+    return true;
+}
+
+bool readlong(SOCKET sock, long *value)
+{
+    if (!readdata(sock, value, sizeof(value)))
+        return false;
+    *value = ntohl(*value);
+    return true;
+}
+
+bool readfile(SOCKET sock, FILE *f)
+{
+    long filesize;
+    if (!readlong(sock, &filesize))
+        return false;
+    if (filesize > 0)
+    {
+        char buffer[1024];
+        do
+        {
+            int num = min(filesize, sizeof(buffer));
+            if (!readdata(sock, buffer, num))
+                return false;
+            int offset = 0;
+            do
+            {
+                size_t written = fwrite(&buffer[offset], 1, num-offset, f);
+                if (written < 1)
+                    return false;
+                offset += written;
+            }
+            while (offset < num);
+            filesize -= num;
+        }
+        while (filesize > 0);
+    }
+    return true;
+}
+
+int __cdecl main(void) 
+{
+    WSADATA wsaData;
+    int iResult;
+
+    SOCKET ListenSocket = INVALID_SOCKET;
+    SOCKET ClientSocket = INVALID_SOCKET;
+
+    struct addrinfo *result = NULL;
+    struct addrinfo hints;
+
+    int iSendResult;
+    char recvbuf[DEFAULT_BUFLEN];
+    int recvbuflen = DEFAULT_BUFLEN;
+    
+    // Initialize Winsock
+    iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+    if (iResult != 0) {
+        printf("WSAStartup failed with error: %d\n", iResult);
+        return 1;
+    }
+
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_flags = AI_PASSIVE;
+
+    // Resolve the server address and port
+    iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+    if ( iResult != 0 ) {
+        printf("getaddrinfo failed with error: %d\n", iResult);
+        WSACleanup();
+        return 1;
+    }
+
+    // Create a SOCKET for connecting to server
+    ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if (ListenSocket == INVALID_SOCKET) {
+        printf("socket failed with error: %ld\n", WSAGetLastError());
+        freeaddrinfo(result);
+        WSACleanup();
+        return 1;
+    }
+
+    // Setup the TCP listening socket
+    iResult = bind( ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+    if (iResult == SOCKET_ERROR) {
+        printf("bind failed with error: %d\n", WSAGetLastError());
+        freeaddrinfo(result);
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    freeaddrinfo(result);
+
+    iResult = listen(ListenSocket, SOMAXCONN);
+    if (iResult == SOCKET_ERROR) {
+        printf("listen failed with error: %d\n", WSAGetLastError());
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    // Accept a client socket
+    ClientSocket = accept(ListenSocket, NULL, NULL);
+    if (ClientSocket == INVALID_SOCKET) {
+        printf("accept failed with error: %d\n", WSAGetLastError());
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    // No longer need server socket
+    closesocket(ListenSocket);
+
+    // Receive until the peer shuts down the connection
+    do {
+		FILE *filehandle = fopen("imagefile.jpg", "wb");
+	if (filehandle != NULL)
+	{
+		bool ok = readfile(ClientSocket, filehandle);
+		fclose(filehandle);
+
+		if (ok)
+		{
+			// use file as needed...
+		}
+		else
+			remove("imagefile.jpg");
+	}
+/*
+        iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
+        if (iResult > 0) {
+            printf("Bytes received: %d\n", iResult);
+
+        // Echo the buffer back to the sender
+            iSendResult = send( ClientSocket, recvbuf, iResult, 0 );
+            if (iSendResult == SOCKET_ERROR) {
+                printf("send failed with error: %d\n", WSAGetLastError());
+                closesocket(ClientSocket);
+                WSACleanup();
+                return 1;
+            }
+            printf("Bytes sent: %d\n", iSendResult);
+        }*/
+        else if (iResult == 0)
+            printf("Connection closing...\n");
+        else  {
+            printf("recv failed with error: %d\n", WSAGetLastError());
+            closesocket(ClientSocket);
+            WSACleanup();
+            return 1;
+        }
+
+    } while (iResult > 0);
+
+    // shutdown the connection since we're done
+    iResult = shutdown(ClientSocket, SD_SEND);
+    if (iResult == SOCKET_ERROR) {
+        printf("shutdown failed with error: %d\n", WSAGetLastError());
+        closesocket(ClientSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    // cleanup
+    closesocket(ClientSocket);
+    WSACleanup();
+
+    return 0;
 }
